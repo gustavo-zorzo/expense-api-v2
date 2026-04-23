4b01f46bc2a350fdb434df941333eac793554979
Gustavo Zorzo <gustavo.zorzo@v4company.com>
2026-04-23 14:49:44 -0300

    feat: add custom error handler in Fastify server

--- infra/server/index.ts
+++ infra/server/index.ts
@@ -1,11 +1,19 @@
 import "reflect-metadata";
-import Fastify from "fastify";
+import Fastify, { type FastifyError, type FastifyReply, type FastifyRequest } from "fastify";
 import { container } from "../di/container.js";
 import { TYPES } from "../../app/di/types.js";
 import { ExpenseController } from "../controller/ExpenseController.js";
+import { ExpenseNotFoundException } from "../../app/exceptions/ExpenseNotFoundException.js";
 
 async function main() {
   const fastify = Fastify({ logger: true });
+  fastify.setErrorHandler((error: FastifyError, request: FastifyRequest, reply: FastifyReply) => {
+    if (error instanceof ExpenseNotFoundException) {
+      reply.status(404).send({ error: error.name, message: error.message });
+    } else {
+      reply.status(500).send({ error: error.name, message: error.message });
+    }
+  });
   const expenseController = container.get<ExpenseController>(TYPES.ExpenseController);
   expenseController.registerRouter(fastify);
   const port = process.env.PORT ? Number(process.env.PORT) : 3008;
